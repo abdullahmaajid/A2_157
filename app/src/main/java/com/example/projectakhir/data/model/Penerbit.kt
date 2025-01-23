e7909bfd1127e5cf101512db556d513bfccba8f0
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2025-01-24 04:45:38 +0700

    membuat model penerbit

---
+++ app/src/main/java/com/example/projectakhir/data/model/Penerbit.kt
@@ -0,0 +1,19 @@
+package com.example.projectakhir.data.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Penerbit(
+    @SerialName("id_penerbit")
+    val idPenerbit: Int,
+
+    @SerialName("nama_penerbit")
+    val namaPenerbit: String,
+
+    @SerialName("alamat_penerbit")
+    val alamatPenerbit: String,
+
+    @SerialName("telepon_penerbit")
+    val teleponPenerbit: String
+)
