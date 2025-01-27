0dcb7317bf362909e0357ef3dd578dc0857a5b90
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2025-01-27 19:17:42 +0700

    memperbarui buku service

--- app/src/main/java/com/example/projectakhir/data/service/BukuService.kt
+++ app/src/main/java/com/example/projectakhir/data/service/BukuService.kt
@@ -1,7 +1,12 @@
 package com.example.projectakhir.data.service
 
 
+
+
 import com.example.projectakhir.data.model.Buku
+import com.example.projectakhir.data.model.Kategori
+import com.example.projectakhir.data.model.Penulis
+import com.example.projectakhir.data.model.Penerbit
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -10,25 +15,29 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface BukuService {
     @Headers(
         "Accept: application/json",
         "Content-Type: application/json",
     )
+
+    // Buku Endpoints
     @GET("api/buku")
-    suspend fun getBuku(): List<Buku>
+    suspend fun getBuku(): Response<List<Buku>>
 
     @GET("api/buku/{id}")
-    suspend fun getBukuById(@Path("id") idBuku: Int): Buku
+    suspend fun getBukuById(@Path("id") idBuku: Int): Response<Buku>
 
     @POST("api/buku")
-    suspend fun insertBuku(@Body buku: Buku)
+    suspend fun insertBuku(@Body buku: Buku): Response<Buku>
 
     @PUT("api/buku/{id}")
-    suspend fun updateBuku(@Path("id") idBuku: Int, @Body buku: Buku)
+    suspend fun updateBuku(@Path("id") idBuku: Int, @Body buku: Buku): Response<Buku>
 
     @DELETE("api/buku/{id}")
     suspend fun deleteBuku(@Path("id") idBuku: Int): Response<Void>
+
+
 }
+
