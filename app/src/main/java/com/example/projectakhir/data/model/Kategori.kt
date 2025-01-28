44699e231e72d568a384d81a5d615015b8a057de
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2025-01-28 22:39:13 +0700

    menambahkan commandline petunjuk

--- app/src/main/java/com/example/projectakhir/data/model/Kategori.kt
+++ app/src/main/java/com/example/projectakhir/data/model/Kategori.kt
@@ -1,17 +1,19 @@
+// Mendeklarasikan package tempat file ini berada
 package com.example.projectakhir.data.model
 
-
+// Mengimpor anotasi untuk serialisasi JSON menggunakan kotlinx.serialization
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+// Memberikan anotasi @Serializable untuk membuat kelas ini dapat diserialisasi (digunakan dengan JSON)
 @Serializable
-data class Kategori(
-    @SerialName("id_kategori")
-    val idKategori: Int,
+data class Kategori( // Mendefinisikan data class bernama Kategori
+    @SerialName("id_kategori") // Menentukan nama properti JSON yang terkait dengan properti ini
+    val idKategori: Int, // Properti untuk menyimpan ID kategori, tipe datanya adalah Int (angka)
 
-    @SerialName("nama_kategori")
-    val namaKategori: String,
+    @SerialName("nama_kategori") // Menentukan nama properti JSON yang terkait dengan properti ini
+    val namaKategori: String, // Properti untuk menyimpan nama kategori, tipe datanya adalah String (teks)
 
-    @SerialName("deskripsi_kategori")
-    val deskripsiKategori: String
+    @SerialName("deskripsi_kategori") // Menentukan nama properti JSON yang terkait dengan properti ini
+    val deskripsiKategori: String // Properti untuk menyimpan deskripsi kategori, tipe datanya adalah String
 )
