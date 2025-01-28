188f4693b13afa29ef1a328cbd6b69e1c088f13a
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2025-01-28 22:39:34 +0700

    menambahkan commandline petunjuk

--- app/src/main/java/com/example/projectakhir/data/applications/PerpustakaanApplications.kt
+++ app/src/main/java/com/example/projectakhir/data/applications/PerpustakaanApplications.kt
@@ -5,13 +5,19 @@ import android.app.Application
 import com.example.projectakhir.data.container.AppContainer
 import com.example.projectakhir.data.container.PerpustakaanContainer
 
+// Mendeklarasikan kelas utama aplikasi yang mewarisi kelas Application
 class PerpustakaanApplications : Application() {
-    // Properti untuk menyimpan instance container
+
+    // Properti untuk menyimpan instance dari AppContainer
+    // Menggunakan lateinit agar properti diinisialisasi nanti
     lateinit var container: AppContainer
 
+    // Fungsi override yang akan dipanggil saat aplikasi dimulai
     override fun onCreate() {
+        // Memanggil implementasi bawaan dari onCreate di kelas Application
         super.onCreate()
-        // Inisialisasi container saat aplikasi dimulai
+
+        // Inisialisasi container dengan instance dari PerpustakaanContainer
         container = PerpustakaanContainer()
     }
 }
